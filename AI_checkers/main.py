ea9cf573c51696f90685d1f34d0f936a75cae446
mskaa3 <j.moskaaa@gmail.com>
2022-04-17 23:47:22 +0200

    basic movement

--- AI_checkers/main.py
+++ AI_checkers/main.py
@@ -1,10 +1,17 @@
 import pygame
-from checkers.constants import width,height
+from checkers.constants import width,height,squares_size
 from checkers.board import Board
 
 FPS=60
 display=pygame.display.set_mode((width,height))
 pygame.display.set_caption('AI_Chekers')
+
+
+def get_from_mouse(position):
+    x,y=position
+    row=y//squares_size
+    column=x//squares_size
+    return row,column
 
 if __name__ == '__main__':
     run=True
@@ -19,7 +26,11 @@ if __name__ == '__main__':
                 #ending the game
                 run=False
             if event.type==pygame.MOUSEBUTTONDOWN:
-                pass
+                position=pygame.mouse.get_pos()
+                row,column=get_from_mouse(position)
+                piece=game_board.get_piece(row,column)
+                game_board.move(piece,4,3)
+                
         game_board.draw(display)
         pygame.display.update()
 
